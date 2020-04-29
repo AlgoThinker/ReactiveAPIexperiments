f3cb09a93a23aa5a31f730b43784f79c668d1125
Tushar Sharma <tushar123cse@gmail.com>
2020-04-29 16:11:02 +0530

    returning sum of flux values

--- callerservice/src/main/java/com/webfluxpoc/callerservice/controller/AggregatorController.java
+++ callerservice/src/main/java/com/webfluxpoc/callerservice/controller/AggregatorController.java
@@ -5,25 +5,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.awt.*;
+import java.util.stream.Collectors;
 
 @RestController
 public class AggregatorController {
 
     @GetMapping(value = "/client/retrieve", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-    public Flux<Integer> getItems(){
-        return Flux.merge(fetchIntgersFromService(), fetchIntgersFromService1());
+    public Mono<Integer> getItems() {
+        Flux<Integer> flux = Flux.merge(fetchIntgersFromService(), fetchIntgersFromService1());
+        Mono<Integer> mono = flux.reduce(0, (a, b) -> a + b);
+        return mono;
     }
 
-   public Flux<Integer> fetchIntgersFromService(){
-       WebClient webClient = WebClient.create("http://localhost:8080");
+    public Flux<Integer> fetchIntgersFromService() {
+        WebClient webClient = WebClient.create("http://localhost:8080");
         return webClient.get().uri("/fluxstream").
                 retrieve().bodyToFlux(Integer.class).log("Items in api service");
-   }
+    }
 
-    public Flux<Integer> fetchIntgersFromService1(){
+    public Flux<Integer> fetchIntgersFromService1() {
         WebClient webClient1 = WebClient.create("http://localhost:8081");
         return webClient1.get().uri("/fluxstream1").
                 retrieve().bodyToFlux(Integer.class).log("Items in api service 1");
