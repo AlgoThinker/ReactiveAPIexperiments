3e604e43712fec5f3e849faf030048b5cffde7a5
Tushar Sharma <tushar123cse@gmail.com>
2020-04-29 15:15:10 +0530

    parallel calls to two microservices

--- callerservice/src/main/java/com/webfluxpoc/callerservice/controller/AggregatorController.java
+++ callerservice/src/main/java/com/webfluxpoc/callerservice/controller/AggregatorController.java
@@ -5,16 +5,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 import java.awt.*;
 
 @RestController
 public class AggregatorController {
 
-    WebClient webClient = WebClient.create("http://localhost:8080");
-
     @GetMapping(value = "/client/retrieve", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Integer> getItems(){
-        return webClient.get().uri("/fluxstream").retrieve().bodyToFlux(Integer.class).log("Items in caller service");
+        return Flux.merge(fetchIntgersFromService(), fetchIntgersFromService1())
+                .parallel()
+                .runOn(Schedulers.elastic()).ordered((a,b)->a-b);
     }
+
+   public Flux<Integer> fetchIntgersFromService(){
+       WebClient webClient = WebClient.create("http://localhost:8080");
+        return webClient.get().uri("/fluxstream").
+                retrieve().bodyToFlux(Integer.class).log("Items in api service");
+   }
+
+    public Flux<Integer> fetchIntgersFromService1(){
+        WebClient webClient1 = WebClient.create("http://localhost:8081");
+        return webClient1.get().uri("/fluxstream1").
+                retrieve().bodyToFlux(Integer.class).log("Items in api service 1");
+    }
+
+
 }
